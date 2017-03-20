287c6188690abe1d459fcba9d7947bcd1a7132dc
hwangshannon <hwangshannon@yahoo.com>
2017-03-20 18:57:16 -0400

    Added "create goal" button & page

--- app/src/main/java/com/example/houssam/empathways/MainActivity.java
+++ app/src/main/java/com/example/houssam/empathways/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.houssam.empathways;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the createGoal button */
+    public void createGoal(View view) {
+        Intent intent = new Intent(this, createGoal.class);
+        startActivity(intent);
     }
 }
