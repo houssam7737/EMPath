b2b2e20f69016f4e69e45fd13a5e1cff58cf6d88
pjlu01 <pjlu@mit.edu>
2017-04-10 10:35:40 -0400

    patricia 4/10

--- app/src/main/java/com/example/houssam/empathways/MainActivity.java
+++ app/src/main/java/com/example/houssam/empathways/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.houssam.empathways;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static com.example.houssam.empathways.R.id.button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
     }
 
-    /** Called when the user taps the createGoal button */
-    public void createGoal(View view) {
-        Intent intent = new Intent(this, createGoal.class);
-        startActivity(intent);
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(context, createGoal.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
+
 }
--- app/src/main/java/com/example/houssam/empathways/createGoal.java
+++ app/src/main/java/com/example/houssam/empathways/createGoal.java
@@ -7,23 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import static com.example.houssam.empathways.R.layout.activity_main;
+
 public class createGoal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_goal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
 
 }
