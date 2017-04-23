fbfe1e6616fb96bfdfcab909459c61d42ee9b58f
Houssam K <houssam@mit.edu>
2017-04-22 22:18:23 -0400

    included some database stuff, but still hasn't closed the loop. Need to get goal and date from inteface and put that in the database

--- app/src/main/java/com/example/houssam/empathways/Database.java
+++ app/src/main/java/com/example/houssam/empathways/Database.java
@@ -1,24 +1,12 @@
 package com.example.houssam.empathways;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
 /**
  * Created by houssam on 4/22/17.
  */
 
-public final class FeedReaderContract {
-    // To prevent someone from accidentally instantiating the contract class,
-    // make the constructor private.
-    private FeedReaderContract() {}
-
-    /* Inner class that defines the table contents */
-    public static class FeedEntry implements BaseColumns {
-        public static final String TABLE_NAME = "entry";
-        public static final String COLUMN_NAME_TITLE = "title";
-        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
-    }
-}
 
 
-public class Database {
-
-
-}
--- app/src/main/java/com/example/houssam/empathways/createGoal.java
+++ app/src/main/java/com/example/houssam/empathways/createGoal.java
@@ -1,5 +1,8 @@
 package com.example.houssam.empathways;
-
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,17 +10,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import static com.example.houssam.empathways.R.attr.subtitle;
+import static com.example.houssam.empathways.R.attr.title;
 import static com.example.houssam.empathways.R.layout.activity_main;
+import com.example.houssam.empathways.FeedReaderDbHelper;
 
 public class createGoal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_goal);
-
-
+        setContentView(R.layout.content_create_goal);
 
     }
 
+    //Method called once user clicks on button to create goal
+    //Input in database the goal and date (goal only for now)
+
+    public void inputInDatabase(View view){
+        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(createGoal.this);
+
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, title);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, subtitle);
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert("GoalsDataBase", null, values);
+
+        
+    }
 }
